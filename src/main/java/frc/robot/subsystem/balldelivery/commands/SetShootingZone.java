56bcd7a2da29566b0e21f0df330ac41fd22834a3
worthp <worthp@worthconsulting.com>
2021-03-08 20:53:48 -0500

    framing setting up shooting positions

---
+++ src/main/java/frc/robot/subsystem/balldelivery/commands/SetShootingZone.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystem.balldelivery.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystem.balldelivery.BallDelivery;
+
+public class SetShootingZone extends CommandBase {
+  private BallDelivery.ShootingZone zone;
+  private BallDelivery  bd;
+  /** Creates a new AngleHoodToPosition. */
+  public SetShootingZone(BallDelivery s, BallDelivery.ShootingZone z) {
+    addRequirements(s);
+    bd  = s;
+    zone  = z;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    bd.setShootingZone(zone);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
